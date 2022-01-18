c024a745f8bf4cd9e3f2075e12bf249614f33409
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-01-18 11:46:02 -0600

    add player test functionality

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,10 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+
+jest.mock('../lib/Potion');
+
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
